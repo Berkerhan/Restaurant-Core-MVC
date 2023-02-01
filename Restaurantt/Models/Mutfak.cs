43b13a4ce688ec8fadd0bebe20e1e8317dae9e09
Berkerhan <berkerhan001@hotmail.com>
2023-02-01 20:27:35 +0300

    commit

---
+++ Restaurantt/Models/Mutfak.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurantt.Models
+{
+    public class Mutfak
+    {
+        public int SefId { get; set; }
+        public string SefAdi { get; set; }
+    }
+}
